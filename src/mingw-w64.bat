a25f76678fa48746fb320bfd369f860f3d2fc273
Bijman <cromalk@protonmail.com>
2025-01-03 15:55:53 +0100

    SRB2SMASHBRAWL release

---
+++ src/mingw-w64.bat
@@ -0,0 +1,6 @@
+echo off
+set PATH=C:\Program Files (x86)\mingw-w64\i686-7.2.0-win32-dwarf-rt_v5-rev1\C:\Program Files (x86)\mingw-w64\i686-7.2.0-win32-dwarf-rt_v5-rev1\bin;%PATH%
+rem echo %PATH%
+rem cd "C:\Program Files (x86)\mingw-w64\i686-7.2.0-win32-dwarf-rt_v5-rev1\C:\Program Files (x86)\mingw-w64\i686-7.2.0-win32-dwarf-rt_v5-rev1\bin"
+cd "C:\"
+"C:\Windows\system32\cmd.exe"
